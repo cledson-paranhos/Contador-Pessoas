67f5706be47ff292b3a4052015ae759095ff479f
Cledson Paranhos <cledsonvgp@hotmail.com>
2021-02-15 21:18:47 -0300

    Iniciando o projeto no GitHub

---
+++ android/app/src/main/java/paranhos/com/contador_de_pessoas/MainActivity.java
@@ -0,0 +1,6 @@
+package paranhos.com.contador_de_pessoas;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Contador de pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String info = "Pode Entrar!";
+
+  void changePeople(int increment) {
+    _people += increment;
+
+    setState(() {
+      if (_people < 0.0) {
+        info = "Mundo invertido !?";
+      } else if (_people <= 10) {
+        info = "Pode Entrar";
+      } else {
+        info = "Atenção! Lotado.";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          "images/restaurante.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pessoa: $_people",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  decoration: TextDecoration.none),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(color: Colors.white, fontSize: 40.0),
+                      ),
+                      onPressed: () {
+                        changePeople(1);
+                      }),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(color: Colors.white, fontSize: 40.0),
+                      ),
+                      onPressed: () {
+                        changePeople(-1);
+                      }),
+                ),
+              ],
+            ),
+            Text(
+              "$info",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 30.0),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
